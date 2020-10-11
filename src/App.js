45ef16eb32ddc75a38f2d36cf5fe5a9cada07d98
nassim <nass_benab@hotmail.com>
2020-10-11 09:17:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from 'react-bootstrap/Form'
+import { Button, Jumbotron} from 'react-bootstrap'; 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+    <div> 
+      <Jumbotron>
+        <h1>Welcome, world!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      This is a simple hero unit, a simple jumbotron-style component for calling
+      extra attention to featured content or information.
+       </p>
+      </Jumbotron>
     </div>
+  <div className='container'>
+    <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+<Form.Group controlId="formBasicPassword">
+  <Form.Label>Password</Form.Label>
+  <Form.Control type="password" placeholder="Password" />
+</Form.Group>
+<Form.Group controlId="formBasicCheckbox">
+  <Form.Check type="checkbox" label="Check me out" />
+</Form.Group>
+<Button variant="warning" type="submit">
+  Submit
+</Button>
+</Form>
+</div>
+</div>
   );
 }
 
